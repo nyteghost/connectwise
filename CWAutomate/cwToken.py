00dd88f79c3916736ceca90bc1c00c14de271b48
nyteghost <nytemmo@gmail.com>
2022-10-01 08:47:17 -0400

    Update cwToken.py

--- CWAutomate/cwToken.py
+++ CWAutomate/cwToken.py
@@ -13,7 +13,7 @@ config=tangerine()
 ### Config
 tokenHeader = config['cwaHeader']
 cwURL = 'https://api-na.myconnectwise.net/v2021_3/apis/3.0/'
-cwAURL = 'https://sca-atl.hostedrmm.com/cwa/api/v1/'
+cwAURL = 'https://<company>.hostedrmm.com/cwa/api/v1/'
 
 
 def getToken():
